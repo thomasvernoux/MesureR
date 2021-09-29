f5c29206cb1e6c2815e980f638194129669833ed
totoaker <71326141+totoaker@users.noreply.github.com>
2021-09-29 16:19:49 +0200

    v finie 01

--- Mesure_resistance/Mesure_resistance.ino
+++ Mesure_resistance/Mesure_resistance.ino
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <String.h>
 #include <Arduino.h>
+#include <TM1637Display.h>
+
+
+
 
 typedef struct Circuit Circuit;
 struct Circuit
@@ -9,13 +13,13 @@ struct Circuit
   char name[10] = "";
   int PinMesureVcc = 0;
   int pinMesureResistance = 0;
-  int Rshunt = 0;
-  double Rmesure = 0;
+  long int Rshunt = 0;
+  float Rmesure = 0;
   int distance = 0; // distance de la mesure avec la moyenne pour avoir la fiabilité de la mesure
   int MesureBruteVCC = 0;
   int MesureBruteResistance = 0;
-  double MesureeVCC = 0;
-  double MesureResistance = 0;
+  float MesureeVCC = 0;
+  float MesureResistance = 0;
   int pinTransistor = 0;
 };
 
@@ -30,20 +34,25 @@ Circuit *ptrC3 = &C3;
 /* Déclaration des fonctions */
 void Mesure(Circuit *);
 void Constantes_setup();
-void changer_circuit(Circuit *);
 void ouvrir_les_circuits(Circuit *, Circuit *, Circuit *);
 void debug(char txt[20], int var);
-void debugD(char txt[20], double var);
-int MesureOptimale (Circuit *C1, Circuit *C2, Circuit *C3);
+void debugD(char txt[20], float var);
+float MesureOptimale (Circuit *C1, Circuit *C2, Circuit *C3);
+void AffichageEcran(float val);
 
 /* Déclaration des variables */
 int tps = 10; // temporisation en ms entre les changements d'états des circuits
-bool ModeDebug = 1;
+bool ModeDebug = true;
+int pinK = 6; // Pin qui connecte la LED (led des kilo)
+
+// Pines pour l'ecran
+#define CLK 9 //can be any digital pin
+#define DIO 8 //can be any digital pin
 
 void setup()
 {
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pinK, OUTPUT); // led Kilo
   Serial.begin(115200);
 
   Mesure(ptrC1);
@@ -72,6 +81,10 @@ void setup()
   pinMode(C2.pinTransistor, OUTPUT);
   pinMode(C3.pinTransistor, OUTPUT);
   ouvrir_les_circuits(ptrC1, ptrC2, ptrC3);
+
+  // Setup Ecran
+  TM1637Display display(CLK, DIO);
+  display.setBrightness(7);
 }
 
 void loop()
@@ -93,7 +106,7 @@ void loop()
   Mesure(ptrC3);
   ouvrir_les_circuits(ptrC1, ptrC2, ptrC3);
 
-  int MesureFinale = MesureOptimale (ptrC1,ptrC2,ptrC3);
+  float MesureFinale = MesureOptimale (ptrC1,ptrC2,ptrC3);
 
   debug("C1.MesureBruteVCC", C1.MesureBruteVCC);
   debug("C1.MesureBruteResistance", C1.MesureBruteResistance);
@@ -111,6 +124,8 @@ void loop()
   debug("C3.distance", C3.distance);
 
   debugD("MesureFinale", MesureFinale);
+  AffichageEcran(MesureFinale);
+
 
   delay(1000);
 }
@@ -125,32 +140,25 @@ void Mesure(Circuit *C)
   C->MesureBruteVCC = analogRead(C->PinMesureVcc);                     //Mesures brutes
   C->MesureBruteResistance = analogRead(C->pinMesureResistance);
 
-  C->MesureeVCC = (double)C->MesureBruteVCC / 1024 *5;                      //Mesures en V
-  C->MesureResistance = (double)C->MesureBruteResistance / 1024 * 5;
+  C->MesureeVCC = (float)C->MesureBruteVCC / 1024 *5;                      //Mesures en V
+  C->MesureResistance = (float)C->MesureBruteResistance / 1024 * 5;
 
   C->Rmesure = C->MesureResistance / (C->MesureeVCC - C->MesureResistance) * C->Rshunt;
-  C->distance = abs(1024 - C->MesureBruteResistance);
+  C->distance = abs((1024 - C->MesureBruteVCC /2 ) - C->MesureBruteResistance);
 
   debugD("C->MesureeVCC",C->MesureeVCC);
   debugD("C->MesureResistance",C->MesureResistance);
   debugD("C->Rmesure",C->Rmesure);
+
+  if(C->Rmesure < 0){ // possibnle pour les grandes resistances avec un Rshunt petit
+    C->distance = 2000;
+  }
 
   debug("Sortie de la Fonction mesure",0);
 
   return;
 }
 
-/*
-Pas finie
-*/
-void changer_circuit(Circuit *C)
-{
-  if (!strcmp(C->name, "C1"))
-  {
-    //ouvrir_les_circuits(C1,C2,C3);
-    digitalWrite(C->pinTransistor, LOW);
-  }
-}
 
 /*
 Cette fonction ouvre les 3 circuits via les transistors 2n2222
@@ -166,37 +174,29 @@ void ouvrir_les_circuits(Circuit *C1, Circuit *C2, Circuit *C3)
 /*
 Debug pour les int
 */
-void debug(char txt[20], int var)
-{
-
-  if (ModeDebug)
-  {
+void debug(char txt[20], int var){
+  if (ModeDebug){
     Serial.print(txt);
     Serial.print(" : ");
     Serial.println(var);
   }
-
   return;
 }
 
 
 /*
-Debug pour les nombres doubles
+Debug pour les nombres float
 */
-void debugD(char txt[20], double var)
-{
-
-  if (ModeDebug)
-  {
+void debugD(char txt[20], float var){
+  if (ModeDebug){
     Serial.print(txt);
     Serial.print(" : ");
     Serial.println(var);
   }
-
   return;
 }
 
-int MesureOptimale (Circuit *C1, Circuit *C2, Circuit *C3){
+float MesureOptimale (Circuit *C1, Circuit *C2, Circuit *C3){
 
   debug("---///--- Entree dans la fonction MesureOptimale",0);
   debug("C1->distance",C1->distance);
@@ -208,16 +208,55 @@ int MesureOptimale (Circuit *C1, Circuit *C2, Circuit *C3){
   debug("Sortie de la fonction MesureOptimale",0);
 
   if (C1->distance <= C2->distance && C1->distance <= C3->distance){
-    return (int)C1->Rmesure;
+    Serial.println("Circuit 1");
+    debugD("C1->Rmesure",C1->Rmesure);
+    return (float)C1->Rmesure;
   }
   else if (C2->distance <= C1->distance && C2->distance <= C3->distance){
-    return (int)C2->Rmesure;
+    Serial.println("Circuit 2");
+    debugD("C2->Rmesure",C2->Rmesure);
+    return (float)C2->Rmesure;
   }
   else if (C3->distance <= C1->distance && C3->distance <= C2->distance){
-    return (int)C3->Rmesure;
+    Serial.println("Circuit 3");
+    debugD("C3->Rmesure",C3->Rmesure);
+    return (float)C3->Rmesure;
+  }
+  else{
+    Serial.println("Grosse erreur");
   }
   return -1;
 }
+
+
+void AffichageEcran(float val){
+  TM1637Display display(CLK, DIO);
+  if (val<1000){
+    debug("Condition 1",0);
+    digitalWrite(pinK,LOW);
+    display.showNumberDec(val,false,4);
+
+  }
+  else if (val < 10000){
+    debug("Condition 2",0);
+    digitalWrite(pinK,HIGH);
+    float valD = (float) val; // val decimale
+    valD /= 10;
+    display.showNumberDecEx(valD, 0b01000000, false, 4, 0);
+  }
+  else {
+    debug("Condition 3",0);
+    digitalWrite(pinK,HIGH);
+    val /= 1000;
+    display.showNumberDec(val,false,4);
+  }
+
+  return;
+}
+
+
+
+
 
 
 
